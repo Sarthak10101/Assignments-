4363c6031afd1436031cbd20c1ecc7ab4b18c543
Sarthak Bhattarai <121297144+Sarthak10101@users.noreply.github.com>
2022-12-31 10:23:26 +0545

    Delete assignment2.py

--- assignment2.py
+++
@@ -1,7 +0,0 @@
-x,y,z = input("Enter three numbers :").split()
-if x<y and x<z :
-    print("The number closest to 0 is ",x)
-elif y<z :
-    print("The number closest to 0 is ",y)
-else :
-    print("the number closest to 0 is ",z)
