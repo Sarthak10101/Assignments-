036d9d9d94c84650238aa65a37f1ab147732b346
Sarthak Bhattarai <saarthakbhattarai44@gmail.com>
2023-01-01 21:00:45 +0545

    Assignment 1

--- assignment1.py
+++ assignment1.py
@@ -1,5 +1,13 @@
-n = int(input("Enter any number :"))
+m,n = (input("Enter two numbers :")).split()
+m = int(m)
+n =int(n)
 if n % 2 ==0 :
  print ("The number ",n," is even.")
 else :
     print ("The number ",n," is odd.")
+if m % 2 ==0 :
+ print ("The number ",m," is even.")
+else :
+    print ("The number ",m," is odd.")    
+#for range
+print ("The range of given numbers is", abs(m-n))
